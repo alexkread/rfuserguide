f92295ca7b1d35bed29e8f30ef9a7eb982fc395a
Alex Read <alex_k_read@outlook.com>
2024-10-11 02:18:13 +0300

    Changed the scripting path to not include spaces

---
+++ RF_reST_to_MD_conversion_script/libraries/conversion_file_pickup.py
@@ -0,0 +1,12 @@
+import os
+
+# Builds then returns all file names in a given subfolder underneath the resources folder 
+# OS independent formatted and as full file path + name + extension formats 
+def get_all_file_names(resources_subfolder_name:str) -> list[str]:   
+    source_directory = os.path.join(os.get_exec_path(), "resources", resources_subfolder_name)
+    return os.listdir(source_directory)
+
+# Builds then returns a single given file name in a given subfolder underneath the resources folder 
+# OS independent formatted and as full file path + name + extension format 
+def get_single_file_name(resources_subfolder_name:str, file_name:str) -> str:  
+    return os.path.join(os.get_exec_path(), "resources", resources_subfolder_name, file_name) 
